874d42fcca1d53ed9e6cbca43a44d623451a383c
Pablo Romero <pablo10romero10@gmail.com>
2022-11-29 20:08:48 -0300

    Projeto criado

---
+++ index.js
@@ -0,0 +1,47 @@
+// config inicial
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const app =  express()
+
+
+
+// forma de ler JSON / middlewares
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+)
+
+app.use(express.json())
+
+// rotas da API
+const personRoutes = require('./routes/personRoutes')
+
+app.use('/person', personRoutes)
+
+// rota inicial /  endpoint
+app.get('/', (req, res) => {
+
+    // mostrar req
+
+    res.json({message: 'Oi Express!' })
+})
+
+//mongodb+srv://Pablo:<ufDoxrXpT2DRFnti>@apicluster.lrtgljv.mongodb.net/bancodaapi?retryWrites=true&w=majority
+
+
+// entregar uma porta
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = encodeURIComponent(process.env.DB_PASSWORD)
+
+mongoose
+    .connect(
+    `mongodb+srv://${DB_USER}:${DB_PASSWORD}@apicluster.lrtgljv.mongodb.net/bancodaapi?retryWrites=true&w=majority`
+    )
+    .then(() => {
+    console.log('Conectamos ao MongoDB!')
+    app.listen(3000)
+    })
+    .catch((err) => console.log(err))
+
